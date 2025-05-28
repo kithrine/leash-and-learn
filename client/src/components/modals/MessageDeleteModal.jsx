be586d8ef9f74f7bcead9498cf1e1952a3ec361c
Katherine Tensfeldt <ktensfeldt@perseverenow.org>
2025-05-28 10:52:55 -0700

    Begins to restyle the inbox page with a new table. Adds a delete icon and back button to the Inbox Detail page. Makes sure the messages on the inbox page appear in reversed order.

--- client/src/components/modals/MessageDeleteModal.jsx
+++ client/src/components/modals/MessageDeleteModal.jsx
@@ -1,4 +1,4 @@
-const ContactDeleteModal = ({ setShowContactDeleteModal, handleDeleteContact, contactToDelete }) => {
+const MessageDeleteModal = ({ setShowMessageDeleteModal, handleDeleteMessage, messageToDelete }) => {
   
   return (
 <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -17,19 +17,19 @@ const ContactDeleteModal = ({ setShowContactDeleteModal, handleDeleteContact, co
               </svg>
             </div>
             <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-              <h3 className="text-base font-semibold text-gray-900" id="modal-title">Delete Session</h3>
+              <h3 className="text-base font-semibold text-gray-900" id="modal-title">Delete Message</h3>
               <div className="mt-2">
-                <p className="text-sm text-gray-500">Are you sure you want to delete this session? All of your data will be permanently removed. This action cannot be undone.</p>
+                <p className="text-sm text-gray-500">Are you sure you want to delete this message? All of your data will be permanently removed. This action cannot be undone.</p>
               </div>
             </div>
           </div>
         </div>
         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
           <button 
-          onClick={() => handleDeleteContact(contactToDelete.id)}
+          onClick={() => handleDeleteMessage(messageToDelete.id)}
           type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Delete</button>
           <button 
-          onClick={() => setShowContactDeleteModal(false)}
+          onClick={() => setShowMessageDeleteModal(false)}
           type="button" 
           className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
         </div>
@@ -40,4 +40,4 @@ const ContactDeleteModal = ({ setShowContactDeleteModal, handleDeleteContact, co
   )
 }
 
-export default ContactDeleteModal
+export default MessageDeleteModal
--- client/src/pages/inbox/InboxDetail.jsx
+++ client/src/pages/inbox/InboxDetail.jsx
@@ -2,14 +2,18 @@ import { useState, useEffect } from "react"
 import { useNavigate, useParams } from "react-router"
 import { useSelector, useDispatch } from "react-redux"
 import { toast } from "react-toastify"
-import { getOneContact } from "../../redux/contactSlice"
+import { contactDelete, getOneContact } from "../../redux/contactSlice"
 import Markdown from "react-markdown"
+import DashboardFooter from "../../components/footers/DashboardFooter"
+import MessageDeleteModal from "../../components/modals/MessageDeleteModal"
 
 const InboxDetail = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const { id } = useParams()
   const { contact } = useSelector((state) => state.contact)
+  const [messageToDelete, setMessageToDelete] = useState({})
+  const [showMessageDeleteModal, setShowMessageDeleteModal] = useState(false)
 
   useEffect(() => {
     console.log("Getting one message on Inbox Detail page")
@@ -23,10 +27,46 @@ const InboxDetail = () => {
     )
   }, [contact])
 
+  const handleDeleteMessage = (id) => {
+    dispatch(contactDelete(id))
+    setShowMessageDeleteModal(false)
+    toast.success("Message deleted successfully!")
+    navigate("/inbox")
+
+  }
+
   return (
     <>
-      <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+      <section class="bg-white dark:bg-gray-900 min-h-[89.5vh]">
+        <div class="py-8 mt-10 px-4 mx-auto max-w-2xl lg:py-16">
+          <div className="flex flex-row gap-x-3 py-3">
+            <svg
+              onClick={() => navigate("/inbox")}
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              fill="currentColor"
+              class="bi bi-arrow-left-circle transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer"
+              viewBox="0 0 16 16">
+              <path
+                fill-rule="evenodd"
+                d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+              />
+            </svg>
+            <svg
+              onClick={() => {
+                setMessageToDelete(contact)
+                setShowMessageDeleteModal(true)
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              fill="currentColor"
+              class="bi bi-trash3 transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 cursor-pointer"
+              viewBox="0 0 16 16">
+              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+            </svg>
+          </div>
           <h2 class="mb-2 text-xl font-extrabold leading-none text-gray-900 md:text-2xl dark:text-white">
             {contact.subject}
           </h2>
@@ -63,7 +103,7 @@ const InboxDetail = () => {
               {/* <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">12kg</dd> */}
             </div>
           </dl>
-          <div class="flex items-center space-x-4">
+          {/* <div class="flex items-center space-x-4">
             <button
               type="button"
               class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -97,9 +137,19 @@ const InboxDetail = () => {
               </svg>
               Delete
             </button>
-          </div>
+          </div> */}
         </div>
       </section>
+
+      {showMessageDeleteModal && (
+        <MessageDeleteModal 
+          setShowMessageDeleteModal={setShowMessageDeleteModal}
+          handleDeleteMessage={handleDeleteMessage}
+          messageToDelete={messageToDelete}
+        />
+      )}
+
+      <DashboardFooter />
     </>
   )
 }
